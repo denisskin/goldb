4d609cac3928a87de8bbbb0c721dbe2c6f1ab3cf
denisskin <denis@begom.com>
2017-02-08 14:21:28 +0300

    changed unit-tests

--- storage_test.go
+++ storage_test.go
@@ -1,10 +1,10 @@
 package goldb
 
 import (
-	"errors"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"math/rand"
+	"os"
 	"testing"
 )
 
@@ -15,29 +15,20 @@ const (
 	IdxUserName
 )
 
-func TestTransaction_PutVar(t *testing.T) {
-	store := NewStorage(fmt.Sprintf("/tmp/test-goldb-%x.db", rand.Int()), nil)
+func TestStorage_Close(t *testing.T) {
+	store := NewStorage(fmt.Sprintf("%s/test-goldb-%x.db", os.TempDir(), rand.Int()), nil)
 	defer store.Drop()
 
-	// put vars
-	err := store.Exec(func(tr *Transaction) {
-		tr.PutVar(Key(TabTest, "keyA"), "Alice") // string
-		tr.PutVar(Key(TabTest, "keyB"), 0xB0b)   // int
-	})
+	err1 := store.Close()
+	err2 := store.Close()
 
-	// get vars
-	var v1 string
-	var v2 int
-	store.GetVar(Key(TabTest, "keyA"), &v1)
-	store.GetVar(Key(TabTest, "keyB"), &v2)
-
-	assert.NoError(t, err)
-	assert.Equal(t, "Alice", v1)
-	assert.Equal(t, 0xB0b, v2)
+	assert.NotNil(t, store)
+	assert.NoError(t, err1)
+	assert.NoError(t, err2)
 }
 
 func TestContext_Fetch(t *testing.T) {
-	store := NewStorage(fmt.Sprintf("/tmp/test-goldb-%x.db", rand.Int()), nil)
+	store := NewStorage(fmt.Sprintf("%s/test-goldb-%x.db", os.TempDir(), rand.Int()), nil)
 	defer store.Drop()
 
 	// put data
@@ -63,88 +54,4 @@ func TestContext_Fetch(t *testing.T) {
 	assert.Equal(t, 4, q.NumRows)
 	assert.Equal(t, 2, qA.NumRows)
 	assert.Equal(t, 1, qA2.NumRows)
-}
-
-func TestTransaction_Get(t *testing.T) {
-	store := NewStorage(fmt.Sprintf("/tmp/test-goldb-%x.db", rand.Int()), nil)
-	defer store.Drop()
-
-	var a, b, c struct {
-		v   string
-		ok  bool
-		err error
-	}
-	key := Key(TabTest, "id")
-	err := store.Exec(func(tr *Transaction) {
-		tr.PutVar(key, "Alice")
-
-		// get data from transaction
-		a.ok, a.err = tr.GetVar(key, &a.v)
-
-		// get data from storage (before commit)
-		b.ok, b.err = store.GetVar(key, &b.v)
-	})
-	// get data from storage (after commit)
-	c.ok, c.err = store.GetVar(key, &c.v)
-
-	assert.NoError(t, err)
-	assert.NoError(t, a.err)
-	assert.NoError(t, b.err)
-	assert.NoError(t, c.err)
-	assert.True(t, a.ok)
-	assert.False(t, b.ok)
-	assert.True(t, c.ok)
-	assert.Equal(t, "Alice", a.v)
-	assert.Equal(t, "", b.v)
-	assert.Equal(t, "Alice", c.v)
-}
-
-func TestTransaction_Discard(t *testing.T) {
-	store := NewStorage(fmt.Sprintf("/tmp/test-goldb-%x.db", rand.Int()), nil)
-	defer store.Drop()
-
-	var a, b struct {
-		v   string
-		ok  bool
-		err error
-	}
-	key := Key(TabTest, "id")
-	err := store.Exec(func(tr *Transaction) {
-		tr.PutVar(key, "Alice")
-
-		a.ok, a.err = tr.GetVar(key, &a.v) // get from transaction
-
-		tr.Fail(errors.New("transaction-fail")) // discard transaction
-	})
-	b.ok, b.err = store.GetVar(key, &b.v) // get from storage
-
-	assert.Error(t, err)
-	assert.NoError(t, b.err)
-	assert.True(t, a.ok)
-	assert.False(t, b.ok)
-	assert.Equal(t, "Alice", a.v)
-	assert.Equal(t, "", b.v)
-}
-
-func TestTransaction_SequenceNextVal(t *testing.T) {
-	store := NewStorage(fmt.Sprintf("/tmp/test-goldb-%x.db", rand.Int()), nil)
-	defer store.Drop()
-
-	var a, b, c, d uint64
-
-	store.Exec(func(tr *Transaction) {
-		a = tr.SequenceNextVal(TabTest)
-		b = tr.SequenceNextVal(TabTest)
-	})
-	store.Exec(func(tr *Transaction) {
-		c = tr.SequenceNextVal(TabTest)
-		tr.Fail(errors.New("transaction-fail")) // discard transaction
-	})
-	store.Exec(func(tr *Transaction) {
-		d = tr.SequenceNextVal(TabTest)
-	})
-
-	assert.True(t, b == a+1)
-	assert.True(t, c == b+1)
-	assert.True(t, d == b+1)
 }

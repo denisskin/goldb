7d7ed62c32516882b161415350666f650ededcdb
denisskin <denis@begom.com>
2017-08-05 18:33:08 +0300

    add dump unit-test

---
+++ dump_test.go
@@ -0,0 +1,61 @@
+package goldb
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"testing"
+	"xnet/std/sys"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//------------------------------------
+func TestStorage_Dump(t *testing.T) {
+	store := NewStorage(fmt.Sprintf("%s/test-goldb-%x.db", os.TempDir(), rand.Int()), nil)
+	defer store.Drop()
+	putTestValues(store, 100)
+
+	err := store.Dump(store.dir+".dump", nil)
+
+	t.Logf(`
+		  DB-size: %d
+		Dump-size: %d`,
+		store.Size(), sys.FileSize(store.dir+".dump"),
+	)
+
+	assert.NoError(t, err)
+}
+
+func TestStorage_Restore(t *testing.T) {
+	store1 := NewStorage(fmt.Sprintf("%s/test-goldb-%x.db", os.TempDir(), rand.Int()), nil)
+	store2 := NewStorage(fmt.Sprintf("%s/test-goldb-%x.db", os.TempDir(), rand.Int()), nil)
+	defer store1.Drop()
+	defer store2.Drop()
+	putTestValues(store1, 100)
+	dumpFilePath := store1.dir + ".dump"
+	store1.Dump(dumpFilePath, nil)
+
+	err := store2.Restore(dumpFilePath)
+
+	assert.NoError(t, err)
+
+	store1.Fetch(NewQuery(TestTable), func(key, val1 []byte) error {
+		val2, err := store2.Get(key)
+
+		assert.NoError(t, err)
+		assert.Equal(t, val1, val2)
+		return err
+	})
+}
+
+func putTestValues(store *Storage, n int) {
+	store.Exec(func(tr *Transaction) {
+		for i := 0; i < n; i++ {
+			tr.PutVar(
+				Key(TestTable, "Key", i),
+				fmt.Sprintf("Value %x", i),
+			)
+		}
+	})
+}

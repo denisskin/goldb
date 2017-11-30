5078f8a901abb1923bddfab6f3565d9031edba34
denisskin <denis@begom.com>
2017-11-30 13:29:41 +0300

    suppress error

--- init_darwin_amd64.go
+++ init_darwin_amd64.go
@@ -1,13 +1,8 @@
 package goldb
 
-import (
-	"log"
-	"syscall"
-)
+import "syscall"
 
 func init() {
 	// set limit open files in process
-	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &syscall.Rlimit{999999, 999999}); err != nil {
-		log.Println("!!!ERROR: syscall.Setrlimit-Error: ", err)
-	}
+	syscall.Setrlimit(syscall.RLIMIT_NOFILE, &syscall.Rlimit{999999, 999999})
 }
